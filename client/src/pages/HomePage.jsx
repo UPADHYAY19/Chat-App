0f0af0b56fa3c87adbaef15e9d88fccdb980878a
UPADHYAY19 <sanyam8208826735@gmail.com>
2025-11-15 00:02:36 +0530

    implemented the online and offline status

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
+import { ChatContext } from '../../context/ChatContext'
 
 const HomePage = () => {
-  const [selectedUser, setSelectedUser] = useState(false)
+  const { selectedUser } = useContext(ChatContext)
 
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
@@ -17,9 +18,9 @@ const HomePage = () => {
               : 'md:grid-cols-2'
           }`}
       >
-        <Sidebar/>
-        <ChatContainer/>
-        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <Sidebar />
+        <ChatContainer />
+        <RightSidebar />
       </div>
     </div>
   )
